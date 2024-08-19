c4a21dbbbd131f1e4f9e9b29a2a5953093d871fb
gykunju <alvingikunju@gmail.com>
2024-08-19 11:54:18 +0300

    Importations

--- App.py
+++ App.py
@@ -3,11 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import StackingClassifier
-from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix
 from Model import stack_model
 
 st.write('''
